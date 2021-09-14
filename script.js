63274ddbaf20ace4fa7878e53f86e2f53461c77f
emilianicasio <emilianicasio@gmail.com>
2021-09-15 06:15:34 +1200

    Remove text

--- script.js
+++ script.js
@@ -80,23 +80,6 @@ function completeToDo(event) {
   displayToDos();
 }
 
-// Una vez que llegaste a este punto verificá que todos los tests pasen
-
-
-// **********************************************EXTRA CREDITOS:********************************************** //
-
-/*    Investigá sobre el tipo 'checkbox' del elemento input y realizar lo siguiente en la función 'buildToDo':
-        a) Crer un checkbox en la función 'buildToDo'
-        b) Asignarle como id a dicho checkbox el valor del index y quitar el id del index de toDoText
-        c) Agregarle al checkbox el 'click' event listener de completeToDo y quitárle el event listener a toDoText
-        d) Asignarle la clase 'completeCheckbox' al checkbox
-        e) Dentro del bloque 'if' de la función buildToDo, si es true, setear el atributo 'checked' en true en el checkbox
-        f) Agregar el checkbox sobre el elemento 'toDoShell'
-*/
-// ********************************************** ----------- ********************************************** //
-
-
-
 displayToDos();
 
 
